1e800f05c0d89bdeb86f07863cb13a0c26a62901
Martynas <Martynas@DESKTOP-HTUUCFV>
2023-05-12 20:46:23 +0300

    Add project files.

---
+++ DemoKintamieji/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DemoKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Skaitinių reikšmių kintamieji
+            int skaiciusSveikasis = 7;
+            double skaiciusDouble = 7.5;
+            float skaiciusFloat = 7.5f;
+
+            skaiciusSveikasis = 8;
+
+            // Tekstinės reikšmės
+            char simbolis = '$';
+            string tekstas = "$a 12 bc įčįčę @!%!*";
+            string vardas = "Martynas";
+
+            // Loginis
+            bool arPenktadienis = true;
+            bool arLyja = false;
+
+            Console.WriteLine(vardas);
+            Console.WriteLine(skaiciusSveikasis);
+        }
+    }
+}
