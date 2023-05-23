267e100f01bdfca3959ec8f78f9857d33155ea33
MartynasVCS <martynasvcs@gmail.com>
2023-05-23 20:45:16 +0300

    Add demo code for inputing to console

---
+++ DemoIvedimas/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DemoIvedimas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lietuviškų simbolių rodymas
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Įveskite savo vardą: ");
+            string ivedimas = Console.ReadLine();
+            Console.WriteLine($"Jūsų vardas: {ivedimas}");
+            Console.WriteLine();
+
+            Console.Write("Įveskite savo amžių: ");
+            Console.WriteLine($"Jūsų amžius: {Console.ReadLine()}");
+            Console.WriteLine();
+
+            Console.Write("Įveskite savo ūgį: ");
+            ivedimas = Console.ReadLine();
+            double ugis = Convert.ToDouble(ivedimas);
+            Console.WriteLine($"Iki dviejų metrų ūgio jums trūksta: {2 - ugis}");
+            Console.WriteLine();
+
+            Console.Write("Įveskite savo svorį: ");
+            double svoris = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Jūsų svoris: {svoris}");
+            Console.WriteLine();
+
+            Console.Write("Įveskite auginamų augintinių skaičių: ");
+            Console.WriteLine($"Jūs turite: {Convert.ToInt32(Console.ReadLine())} augintinių");
+            Console.WriteLine();
+        } 
+    }
+}
