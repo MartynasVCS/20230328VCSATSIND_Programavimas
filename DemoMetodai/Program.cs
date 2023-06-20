fcdc33d33f0b8b33fe8353f3979b41b7de0f118a
MartynasVCS <martynasvcs@gmail.com>
2023-06-20 21:37:51 +0300

    Add initial demo code about methods

---
+++ DemoMetodai/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DemoMetodai
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lietuviškų simbolių rodymas
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Pasisveikinimas();
+            Pasisveikinimas();
+            Pasisveikinimas();
+            Pasisveikinimas();
+        }
+
+        static void Pasisveikinimas()
+        {
+            Console.WriteLine("Labas");
+            Console.WriteLine("rytas");
+            Console.WriteLine("!");
+        }
+    }
+}
